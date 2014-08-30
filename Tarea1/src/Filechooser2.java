564b9bcc19f121af4f470a0b3dced3ddaeb65920
joserc269510 <flaco269510@gmail.com>
2014-08-30 13:07:30 -0600

    Revert "Revert "codigos""
    This reverts commit 9d216511f46b4a1328b6bb0c990a1f8c9379b5bd.

---
+++ Tarea1/src/Filechooser2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package filechooser.pkg2;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+
+public class Filechooser2 {
+
+    public static void main(String[] args) {
+        
+        JFileChooser fileChooser = new JFileChooser(".");
+        int status = fileChooser.showOpenDialog (null);
+        if (status == JFileChooser.APPROVE_OPTION){
+            File selectedFile = fileChooser.getSelectedFile();
+            System.out.println (selectedFile.getParent());
+        }
+        else{ 
+            if (status == JFileChooser.CANCEL_OPTION){
+                System.out.println("CANCELAR");
+            }
+            
+        }
+        
+        
+
+    }
+    
+}
