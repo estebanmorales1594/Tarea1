6890cd2483a67766cf0967c36dba8479a64ab473
redhuo <jimmytsafg@gmail.com>
2014-08-30 11:01:34 -0600

    Implemente los Mp3 tags al metodo Guardar Objetos

--- Tarea1/src/metodoGuardarObj.java
+++ Tarea1/src/metodoGuardarObj.java
@@ -1,9 +1,15 @@
 
+import com.mpatric.mp3agic.ID3v1;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
+import java.io.IOException;
 import java.util.ArrayList;
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 Aqui empieza el codigo
  */
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Objeto que se encarga de guardar los objetos canciones en una lista enlazada
 public class metodoGuardarObj {
     
@@ -13,23 +19,25 @@ public class metodoGuardarObj {
     
     static guardarInformacion cancion; 
     static int x;
+    static String camino;
   
-    
-   public static void modificarCancion(String nombreCancion,String cancion,String artista,String album){
-       
-       
-       
-   }
-   
-    
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //metodo que ingresa las canciones en un array
-    public static void metodoGuardarObj(String Autor,String Album,String Cancion,String Genero,String duracion){
-      
-           cancion = new guardarInformacion( Autor , Album ,Cancion,Genero,duracion);
-           listaCanciones.add(cancion);
+    public static void agregarCancion(String camino) throws IOException, UnsupportedTagException, InvalidDataException{
+        Mp3File mp3file = new Mp3File(camino);   
+        ID3v1 id3v1Tag = mp3file.getId3v1Tag();
+        String Autor = id3v1Tag.getArtist();
+        String Album = id3v1Tag.getAlbum();
+        String Cancion = id3v1Tag.getTitle();
+        String Genero = id3v1Tag.getGenreDescription();
+        long Duracion = mp3file.getLengthInSeconds();
+        
+        cancion = new guardarInformacion( Autor , Album ,Cancion,Genero,Duracion+"");
+        listaCanciones.add(cancion);
        
        
     }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //muestra la informacion deseada  
     public  static void mostrarCancion(String nombreCancion){      
         for(int i = 0; i< listaCanciones.size(); i++){
@@ -43,6 +51,7 @@ public class metodoGuardarObj {
             
         }
     }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Metodo de busqueda secuencial usando un indice
     public static Object mostrarBusqueda(String elemento){
         
@@ -53,27 +62,26 @@ public class metodoGuardarObj {
             if (listaCanciones.get(x).getArtista() == elemento){
                
                listaBusqueda.add(listaCanciones.get(x).getCancion());
-            
-
             }
-            x++;
-          
-            
+            x++;       
         } 
         System.out.println(listaBusqueda);
         return (listaBusqueda); 
         
     }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void eliminarCancion(){
         
     }
-    
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //Prueba de los metodos.
-    public static void main (String[] args){
-        metodoGuardarObj("Jim Carrey","Los D","Dos de Queso","Blues","2:30");
-        metodoGuardarObj("Jim Carrey","Los D","Blues","Blues","2:30");
-        mostrarCancion("Dos de Queso");
-        mostrarBusqueda("Jim Carrey");
+    // Aqui comienza el main.///////////////////////////////////////////////////////////////////////////////////////
+    public static void main (String[] args) throws IOException, UnsupportedTagException, InvalidDataException{
+        agregarCancion("C:\\Users\\Jimmy\\Downloads\\ow9nwithywithme\\01. With You-With Me.mp3");
+        System.out.println(listaCanciones.get(0).getCancion());
+        
+  
+     
     }
 }
 
