9aa2bb1e979ae058bb7b8ce5e33d8cb899ce0b92
estebanmorales1594 <estebanmorales1594@hotmail.com>
2014-09-03 22:20:11 -0600

    clean
    Les Borre las cosas que decian @autor

--- Tarea1/src/mp3Player.java
+++ Tarea1/src/mp3Player.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Jimmy
- */
 public class mp3Player extends javax.swing.JFrame {
 
     /**
